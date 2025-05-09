5b2786b521facac299e1e0c13539cfcbadb62e4e
Rasoproject <valdemarambr@gmail.com>
2025-05-10 00:33:30 +0100

    Add files via upload
    Vamos tentar mais uma vez

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Olá, este é o Meu SaaS 🚀</h1>
+      <p>Deploy com Vite + React funcionando!</p>
+    </div>
+  )
+}
+
+export default App
